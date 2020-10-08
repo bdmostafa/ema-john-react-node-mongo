362a8e1c069f876ae751a9619951bc9a312e2caa
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-10-08 20:43:23 +0600

    stripe api

--- ema-john-client/src/components/PaymentProcess/PaymentProcess.js
+++ ema-john-client/src/components/PaymentProcess/PaymentProcess.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-// import SimpleCardForm from './SimpleCardForm';
 import SplitCardForm from './SplitCardForm';
 import SimpleCardForm from './SimpleCardForm';
+import { stripe_API } from './stripe.config';
 
-const stripePromise = loadStripe('pk_test_51HZnnCGEVOGm2sOALIGlD5pvadMIVHOUNzG6Sjh7Ur3Y6RjXBXDRfuZRTmpZYXAuzhnBEWjA5DESxDlyE1KKS6Hs00tenH1Gr5');
+const stripePromise = loadStripe(stripe_API);
 
 const PaymentProcess = ({ handlePayment }) => {
     return (
